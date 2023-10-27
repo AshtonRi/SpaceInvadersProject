148fd4ef8c429659bb8cab0b3234a9ed7b04f9d8
Ashton Riblett <riblettashton@gmail.com>
2023-10-26 20:43:11 -0700

    first commit

---
+++ menu.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("myButton");
+
+        button.addEventListener("click", function() {
+            // Redirect to the Wikipedia website
+            window.location.href = "https://en.wikipedia.org/";
+        });
+
